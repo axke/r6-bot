b5a741cec0e5795a96d7728aa64a3aee2041e5ab
axke <ackempf@eagles.usi.edu>
2018-12-03 18:53:32 -0600

    typo

--- commands/generation/match.js
+++ commands/generation/match.js
@@ -32,7 +32,7 @@ module.exports = class MatchCommand extends Commando.Command {
         const middle = Math.ceil(players.length / 2);
         let embed = new RichEmbed()
             .setAuthor(`R6 Team Generation`)
-            .addField(`Blue Team`, `${tplayers.splice(0, middle)}`, false)
+            .addField(`Blue Team`, `${players.splice(0, middle)}`, false)
             .addField(`Orange Team`, `${players.splice(middle, players.length)}`, false);
         // figure out the map
         const maps = [
