c7ea0ddeefa9875b0784c0521733b4b3480969df
alexkempf <ackempf@eagles.usi.edu>
2019-08-08 10:32:18 -0500

    comp bug

--- commands/generation/composition.js
+++ commands/generation/composition.js
@@ -31,17 +31,20 @@ module.exports = class CompositionCommand extends Commando.Command {
             'Frost', 'Valkyrie', 'Caviera', 'Echo', 'Mira', 'Ela', 'Lesion', 'Vigil', 'Alibi', 'Maestro', 'Clash', 
             'Kaid', 'Mozzie', 'Smoke', 'Mute', 'Castle', 'Pulse', 'Doc', 'Rook', 'Kapkan', 'Tachanka', 'Jager', 'Bandit',            
         ];
+
+        const attackTeam = this.fiveRandoms(attack);
+        const defenseTeam = this.fiveRandoms(defense);
+        
         let embed = new RichEmbed()
             .setAuthor(`R6 Team Composition Generator`)
-            .addField(`Attack`, this.arrayToList(attack), false)
-            .addField(`Defense`, this.arrayToList(defense), false);        
+            .addField(`Attack`, this.arrayToList(attackTeam), false)
+            .addField(`Defense`, this.arrayToList(defenseTeam), false);        
         return embed;
     }
 
     fiveRandoms(operators) {
         let randoms = [];
-        c = 5;
-        n = 0;
+        let c = 5;
         for (let n = 0; n < c; n++) {
             let num = Math.floor(Math.random() * operators.length - n);
             n = Math.min(n + 1, c);
